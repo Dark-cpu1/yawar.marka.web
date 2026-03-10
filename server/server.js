f009c1d2e2928690e892edbf9af4bc2d49e715d4
Dark-cpu1 <bryancolcasantibanes2@gmail.com>
2026-03-10 13:15:46 -0500

    cambio de server

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(cors({
 app.use(express.json())
 
 app.get("/", (req, res) => {
-  res.send("Backend funcionando 🚀")
+  res.send("Backend funcionando ")
 })
 
 if (!fs.existsSync("uploads")) {
