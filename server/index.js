271b06546c7c0fe043afb93aad561801fb028d97
Dark-cpu1 <bryancolcasantibanes2@gmail.com>
2026-03-10 22:38:31 -0500

    cambio

--- server/index.js
+++ server/index.js
@@ -14,13 +14,12 @@ app.use(cors({
 app.use(express.json())
 
 const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  host: process.env.MYSQLHOST,    
+  user: process.env.MYSQLUSER,    
+  password: process.env.MYSQLPASSWORD, 
+  database: process.env.MYSQLDATABASE, 
+  port: process.env.MYSQLPORT      
 })
-
 
 db.connect((err) => {
   if (err) {
--- server/server.js
+++ server/server.js
@@ -29,11 +29,11 @@ require("dotenv").config()
 
 
 const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  host: process.env.MYSQLHOST,    
+  user: process.env.MYSQLUSER,   
+  password: process.env.MYSQLPASSWORD, 
+  database: process.env.MYSQLDATABASE, 
+  port: process.env.MYSQLPORT     
 })
 
 db.connect((err) => {
